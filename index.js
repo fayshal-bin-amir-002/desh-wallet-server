f10f3cb62f03a9ac718fa9afc6f3c1a401e4a6ab
fayshal69 <foyshalbinamir@gmail.com>
2024-07-16 19:41:27 +0600

    add: user update func

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
+var jwt = require('jsonwebtoken');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
 require('dotenv').config();
@@ -14,6 +15,7 @@ app.use(express.json());
 const user = process.env.DB_USER;
 const pass = process.env.DB_PASS;
 const saltRounds = 5;
+const secret_access_token = process.env.SECRET_ACCESS_TOKEN;
 
 const uri = `mongodb+srv://${user}:${pass}@cluster0.0hiczfr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -32,6 +34,56 @@ async function run() {
 
         const database = client.db("deshWalletDB");
         const usersCollection = database.collection("users");
+        const sendMoneyCollection = database.collection("sendMoney");
+        const cashInCollection = database.collection("cashIn");
+        const cashOutCollection = database.collection("cashOut");
+
+        //<---middleware for verify token--->
+        const verifyToken = async (req, res, next) => {
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: "Unauthorized Access" });
+            }
+
+            const token = req.headers.authorization.split(' ')[1];
+
+            jwt.verify(token, secret_access_token, (error, decoded) => {
+                if (error) {
+                    return res.status(401).send({ message: "Unauthorized Access" });
+                }
+                req.decoded = decoded;
+
+                next();
+            })
+        }
+
+        //<---middleware for verify admin--->
+        const verifyAdmin = async (req, res, next) => {
+            const user = req.decoded;
+            const query = { email: user?.email };
+            const result = await usersCollection.findOne(query);
+            if (!result || result?.role !== 'admin') {
+                return res.status(401).send({ message: "Unauthorized Access" });
+            }
+            next();
+        }
+
+        //<---middleware for verify agent--->
+        const verifyAgent = async (req, res, next) => {
+            const user = req.decoded;
+            const query = { email: user?.email };
+            const result = await usersCollection.findOne(query);
+            if (!result || result?.role !== 'trainer') {
+                return res.status(401).send({ message: "Unauthorized Access" });
+            }
+            next();
+        }
+
+        //<---jwt token req--->
+        app.post("/jwt", async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, secret_access_token, { expiresIn: "1h" });
+            res.send({ token: token });
+        })
 
         //add new user in db
         app.post("/add-user", async (req, res) => {
@@ -44,7 +96,8 @@ async function run() {
                     ...newUser,
                     pin: hash,
                     status: "pending",
-                    role: 'user'
+                    role: 'user',
+                    balance: 0
                 }
                 const result = await usersCollection.insertOne(user);
                 const regUser = {
@@ -52,7 +105,8 @@ async function run() {
                     email: user?.email,
                     phone: user?.phone,
                     status: user?.status,
-                    role: user.role
+                    role: user.role,
+                    balance: user?.balance
                 }
                 res.send({ result, regUser });
             });
@@ -77,10 +131,88 @@ async function run() {
                     email: dbUser.email,
                     phone: dbUser.phone,
                     status: dbUser.status,
-                    role: dbUser.role
+                    role: dbUser.role,
+                    balance: dbUser.balance
                 }
                 res.send(loggedUser);
             });
+        })
+
+        //get user data from 
+        app.get("/userData/:email", async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const options = {
+                projection: { _id: 0, name: 1, email: 1, phone: 1, status: 1, role: 1, balance: 1 },
+            };
+            const result = await usersCollection.findOne(query, options);
+            res.send(result);
+        })
+
+        //send money to a user
+        app.post("/send-money", async (req, res) => {
+            const sendMoneyData = req.body;
+            console.log(sendMoneyData);
+            const query1 = { phone: sendMoneyData.senderNumber };
+            const query2 = { phone: sendMoneyData.receiverNumber };
+
+            const updateDoc1 = {
+                $inc: {
+                    balance: - sendMoneyData.totalAmout
+                },
+            };
+            const updateDoc2 = {
+                $inc: {
+                    balance: sendMoneyData.sentAmount
+                },
+            };
+
+            const result1 = await usersCollection.updateOne(query1, updateDoc1);
+            const result2 = await usersCollection.updateOne(query2, updateDoc2);
+
+            res.send(result1);
+
+        })
+
+        //get all users
+        app.get("/users", verifyToken, async (req, res) => {
+            const email = req.query.email;
+            const phone = req.query.phone;
+
+            if (req?.decoded?.acc !== email && req?.decoded?.acc !== phone) {
+                return res.status(403).send({ message: 'Forbidden Access' });
+            }
+            const options = {
+                projection: { _id: 1, name: 1, email: 1, phone: 1, status: 1, role: 1, balance: 1 }
+            };
+            const result = await usersCollection.find({}, options).toArray();
+
+            res.send(result);
+        })
+
+        //update a user
+        app.patch("/update-user", verifyToken, async (req, res) => {
+            const email = req.query.email;
+            const phone = req.query.phone;
+
+            if (req?.decoded?.acc !== email && req?.decoded?.acc !== phone) {
+                return res.status(403).send({ message: 'Forbidden Access' });
+            }
+            const { id } = req.body;
+            const query = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    status: "verified"
+                },
+            };
+            const updateDoc1 = {
+                $inc: {
+                    balance: 40
+                },
+            };
+            const result = await usersCollection.updateOne(query, updateDoc);
+            const result1 = await usersCollection.updateOne(query, updateDoc1);
+            res.send(result);
         })
 
         // Send a ping to confirm a successful connection
